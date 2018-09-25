ccd4ed061ac2ee0cecd4775dedab09ec7ec69f66
cathyatseneca <cathyatseneca@gmail.com>
2018-09-25 12:27:43 -0400

    updated with iterator code

--- dlist.h
+++ dlist.h
@@ -24,28 +24,89 @@ public:
 	void print() const;
 	void reversePrint() const;
 	~DList();
+	iterator begin(){
+		return iterator(front_,this);
+	}
+	iterator end(){
+		return iterator(nullptr,this);
+	}
+	const_iterator cbegin() const{
+		return const_iterator(front_,this);
+	}
+	const_iterator cend() const{
+		return const_iterator(nullptr,this);
+	}
 
 	class const_iterator{
+	friend class DList<T>;
+	protected:
 		Node* curr_;
+		DList* theList_;
+		const_iterator(Node* curr, DList* theList){
+			theList_=theList;
+			curr_=curr;
+		}
 	public:
-		const_iterator(){}
-		const_iterator operator++(){}
-		const_iterator operator++(int){}
-		const_iterator operator--(){}
-		const_iterator operator--(int){}
-		bool operator==(const_iterator rhs){}
-		bool operator!=(const_iterator rhs){}
-		const T& operator*()const{}
+		const_iterator(){
+			curr_=nullptr;
+			theList_=nullptr;
+		}
+		//++x
+		const_iterator operator++(){
+			curr_=curr->next_;
+			return *this;
+		}
+
+		//x++
+		const_iterator operator++(int){
+			const_iterator old=*this;
+			curr_=curr_->next_;
+			return old;
+		}
+		const_iterator operator--(){
+			if(curr_ == nullptr){
+				if(theList_!=nullptr){	
+					curr_=theList_->back_;
+				}
+			}
+		}
+		const_iterator operator--(int){
+
+		}
+		bool operator==(const_iterator rhs){
+			bool rc=false;
+			if(theList_ == rhs.theList_ && curr_==rhs.curr_){
+				rc=true;
+			}
+		}
+		bool operator!=(const_iterator rhs){
+			return (!(*this==rhs))
+		}
+		const T& operator*()const{
+			return curr_->data_;
+		}
 	};
 	class iterator:public const_iterator{
+	friend class DList<T>
+	protected:
+		iterator(Node* curr, DList* theList):const_iterator(curr,theList){}
 	public:
-		iterator();
+		iterator(){
+			this->theList_=theList;
+			this->curr_=curr;			
+		}
 		iterator operator++(){}
 		iterator operator++(int){}
 		iterator operator--(){}
 		iterator operator--(int){}
-		T& operator*(){}
-		const T& operator*()const{}
+		T& operator*(){
+			return this->curr_->data_;
+
+		}
+		const T& operator*()const{
+			return this->curr_->data_;
+
+		}
 	};
 
 };
