67b97c0e558c7b30d0fd6523d6f05115ad2d56fd
cathyatseneca <cathyatseneca@gmail.com>
2018-09-25 13:26:51 -0400

    linked list stack update

--- llstack.h
+++ llstack.h
@@ -1,16 +1,21 @@
 #include "dlist.h"
 template <class T>
 class Stack{
-	DList theStack_;
+	DList<T> theStack_;
 
 public:
 	Stack(){}
 	void push(const T& data){
+		theStack_.push_front(data);
 	}
 	void pop(){
+		theStack_.pop_front();
 	}
 	T top() const{
+		const_iterator top=theStack_.cbegin();
+		return *top;
 	}
 	bool isEmpty(){
+		return (begin()==end())
 	}	
 };
